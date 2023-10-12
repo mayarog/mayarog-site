914ff565cfec9f5c5e77489585078cc4f2842444
Marc Reinan Gomes <marcreinan@hotmail.com>
2023-10-12 10:18:09 -0300

    chore(layout): defining positions on titles and image on IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <h2 class="text-h2">Mayarog WEB</h2>
-    <h4 class="text-h4">Sistemas web e Consultoria em T.I.</h4>
-    <br />
     <img
       alt="Mayarog Web"
       src="~assets/mayarog-logo.png"
       style="width: 300px; height: 300px"
     />
+    <h4 class="text-h4">Sistemas web e Consultoria em T.I.</h4>
     <h5><q-icon name="record_voice_over" />+55 (77) 991 100 227</h5>
     <h5><q-icon name="chat" />@mayarog_web</h5>
   </q-page>
