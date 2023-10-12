f56c53741aea3e9d873d34589c6e08f8a12c85cf
Marc Reinan Gomes <marcreinan@hotmail.com>
2023-10-12 09:48:48 -0300

    chore(layout): changed index messaged and included icons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <h2>Mayarog WEB</h2>
+    <h4>Sistemas web e Consultoria em T.I.</h4>
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    > -->
+    <q-icon name="record_voice_over" />+55 (77) 991 100 227
+    <q-icon name="chat" />@mayarog_web
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage"
+});
 </script>
