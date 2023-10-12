e2bbdfbbf005fc26d8ed8d6ec387328dad13d807
Marc Reinan Gomes <marcreinan@hotmail.com>
2023-10-12 09:39:59 -0300

    chore(layout): change color of toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar color="teal-lighten-1">
         <q-btn
           flat
           dense
@@ -11,13 +11,13 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Mayarog Web - teste</q-toolbar-title>
+        <q-toolbar-title> Mayarog Web</q-toolbar-title>
 
         <div>Sistemas e Consultoria</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
 
@@ -27,7 +27,7 @@
           v-bind="link"
         />
       </q-list>
-    </q-drawer>
+    </q-drawer> -->
 
     <q-page-container>
       <router-view />
@@ -37,58 +37,58 @@
 
 <script>
 import { defineComponent, ref } from "vue";
-import EssentialLink from "components/EssentialLink.vue";
+// import EssentialLink from "components/EssentialLink.vue";
 
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev"
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework"
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev"
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev"
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
-];
+// const linksList = [
+//   {
+//     title: "Docs",
+//     caption: "quasar.dev",
+//     icon: "school",
+//     link: "https://quasar.dev"
+//   },
+//   {
+//     title: "Github",
+//     caption: "github.com/quasarframework",
+//     icon: "code",
+//     link: "https://github.com/quasarframework"
+//   },
+//   {
+//     title: "Discord Chat Channel",
+//     caption: "chat.quasar.dev",
+//     icon: "chat",
+//     link: "https://chat.quasar.dev"
+//   },
+//   {
+//     title: "Forum",
+//     caption: "forum.quasar.dev",
+//     icon: "record_voice_over",
+//     link: "https://forum.quasar.dev"
+//   },
+//   {
+//     title: "Twitter",
+//     caption: "@quasarframework",
+//     icon: "rss_feed",
+//     link: "https://twitter.quasar.dev"
+//   },
+//   {
+//     title: "Facebook",
+//     caption: "@QuasarFramework",
+//     icon: "public",
+//     link: "https://facebook.quasar.dev"
+//   },
+//   {
+//     title: "Quasar Awesome",
+//     caption: "Community Quasar projects",
+//     icon: "favorite",
+//     link: "https://awesome.quasar.dev"
+//   }
+// ];
 
 export default defineComponent({
   name: "MainLayout",
 
   components: {
-    EssentialLink
+    // EssentialLink
   },
 
   setup() {
