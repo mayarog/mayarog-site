231cbfed68346f8f72a1233fb7eeacf15c0c3b5c
Marc Reinan Gomes <marcreinan@hotmail.com>
2023-10-12 10:03:49 -0300

    chore(layout): imported and setted mayarog-logo on IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,11 +3,11 @@
     <h2 class="text-h2">Mayarog WEB</h2>
     <h4 class="text-h4">Sistemas web e Consultoria em T.I.</h4>
     <br />
-    <!-- <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    > -->
+    <img
+      alt="Mayarog Web"
+      src="~assets/mayarog-logo.png"
+      style="width: 300px; height: 300px"
+    />
     <h5><q-icon name="record_voice_over" />+55 (77) 991 100 227</h5>
     <h5><q-icon name="chat" />@mayarog_web</h5>
   </q-page>
