16419e3557183f646303575649cb52db9d7c47a9
Marc Reinan Gomes <marcreinan@hotmail.com>
2023-11-04 12:27:44 -0300

    chore(pages): setted toolbar on index page, and removed slogan sentence

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,40 @@
 <template>
-  <div class="fullscreen bg-green text-white text-center">
-    <div class="q-pa-md flex flex-center">
-      <h1 class="q-mb-md text-h1">Mayarog</h1>
-    </div>
-
-    <div class="q-pa-md flex flex-center">
-      <q-img
-        alt="Mayarog Web"
-        src="~assets/mayarog-logo.png"
-        class="fixed-center"
-        style="width: 200px; height: 200px"
-      />
-    </div>
-
-    <div class="q-pa-md flex flex-center fixed-center">
-      <h4 class="text-h4">Sistemas web & consultoria em T.I.</h4>
-    </div>
-    <div class="q-pa-md flex flex-center fixed-bottom">
-      <!-- Seção de contato do WhatsApp -->
+  <q-page class="bg-black text-white text-center">
+    <q-page-container>
       <div class="q-pa-md flex flex-center">
-        <q-btn
-          icon="mdi-whatsapp"
-          color="green"
-          @click="enviarMensagemWhatsApp"
-          label="Contato por WhatsApp"
+        <h1 class="q-mb-md text-h1">Mayarog</h1>
+      </div>
+
+      <div class="q-pa-md flex flex-center">
+        <q-img
+          alt="Mayarog Web"
+          src="~assets/mayarog-logo.png"
+          class="fixed-center"
+          style="width: 200px; height: 200px"
         />
       </div>
-      <!-- Seção de contato do Instagram -->
-      <div class="q-pa-md flex flex-center">
-        <q-btn
-          icon="mdi-instagram"
-          color="primary"
-          @click="irParaInstagram"
-          label="Siga-nos no Instagram"
-        />
+      <div class="q-pa-md flex flex-center fixed-bottom">
+        <!-- Seção de contato do WhatsApp -->
+        <div class="q-pa-md flex flex-center">
+          <q-btn
+            icon="mdi-whatsapp"
+            color="green"
+            @click="enviarMensagemWhatsApp"
+            label="Contato por WhatsApp"
+          />
+        </div>
+        <!-- Seção de contato do Instagram -->
+        <div class="q-pa-md flex flex-center">
+          <q-btn
+            icon="mdi-instagram"
+            color="primary"
+            @click="irParaInstagram"
+            label="Siga-nos no Instagram"
+          />
+        </div>
       </div>
-    </div>
-  </div>
+    </q-page-container>
+  </q-page>
 </template>
 
 <script>
