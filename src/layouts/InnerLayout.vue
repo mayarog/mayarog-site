ba426623141693e1100bfa102875cba48d5e9295
Marc Reinan Gomes <marcreinan@outlook.com>
2024-06-01 12:34:44 -0300

    chore(layouts): created innerLayout Layout

---
+++ src/layouts/InnerLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
+    <TheHeader>
+      <template v-slot:footerToolbar>
+        <q-toolbar inset>
+          <q-breadcrumbs active-color="white" style="font-size: 16px">
+            <q-breadcrumbs-el label="Home" icon="home"></q-breadcrumbs-el>
+            <q-breadcrumbs-el label="Serviços" icon="widgets"></q-breadcrumbs-el>
+            <q-breadcrumbs-el label="Toolbar"></q-breadcrumbs-el>
+          </q-breadcrumbs>
+        </q-toolbar>
+      </template>
+    </TheHeader>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+import TheHeader from "src/components/TheHeader.vue";
+export default defineComponent({
+  name: "MainLayout",
+  components: {
+    TheHeader
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      }
+    };
+  }
+});
+</script>
