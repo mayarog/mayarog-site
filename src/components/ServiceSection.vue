7ec21a3266d9ad8429f9384b534b7d52511be9d5
imLeonam <leonammrib@gmail.com>
2024-06-19 20:05:22 -0300

    refactor(components): changed layout in SerciceSection

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -1,12 +1,7 @@
 <template>
-  <q-page-container id="services" class="bg-black text-white text-center">
-    <div class="flex flex-center">
-      <div class="col-12">
-        <h4 class="text-h5">
-          Oferecemos soluções e consultoria em projetos web <br />
-        </h4>
-        <small class="text-h6"> Conheça nossos principais serviços: </small>
-      </div>
+  <q-page-container id="services" class="bg-dark text-white text-center">
+    <div class="q-my-xl">
+      <small class="text-h5"> Conheça nossos principais serviços: </small>
     </div>
     <div class="row">
       <div
@@ -14,17 +9,28 @@
         v-for="(card, index) in cards"
         :key="card"
       >
-        <q-card class="bg-dark text-white">
+        <q-card
+          class="bg-dark text-white rounded"
+          v-motion-slide-visible-once-bottom
+          flat
+          :delay="index * 120"
+        >
           <q-card-section>
-            <q-icon :name="card.icon" color="secondary" size="6rem"></q-icon>
+            <q-icon :name="card.icon" color="primary" size="6rem"></q-icon>
             <div class="text-h6 q-my-md">{{ card.title }}</div>
             <div class="text-subtitle2 q-my-sm">
               {{ card.subtitle }}
             </div>
             <q-btn
-              class="q-my-md"
+              v-motion
+              :initial="{ opacity: 1, y: 0 }"
+              :enter="{ y: 0, scale: 1.1 }"
+              :hovered="{ scale: 1.2 }"
+              class="q-my-md q-px-lg q-py-sm"
               rounded
               color="primary"
+              size="18px"
+              text-color="dark"
               label="Saiba mais"
               @click="openDialog(index)"
             />
@@ -45,94 +51,85 @@
         </q-card>
       </q-dialog>
     </div>
-    <div class="flex flex-center">
+    <div class="flex flex-center q-mt-xl">
       <q-btn
-        class="q-my-md"
+        v-motion
+        :initial="{ opacity: 0, y: 100 }"
+        :enter="{ opacity: 1, y: 0, scale: 1.1 }"
+        :hovered="{ scale: 1.2 }"
+        class="q-my-md q-px-lg"
         to="/em-construcao"
         rounded
+        outline
         block
-        color="secondary"
+        size="20px"
+        color="primary"
         label="Conheça todos os serviços"
       />
     </div>
   </q-page-container>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
-  name: "ServiceSection",
-  setup() {
-    const cards = ref([
-      {
-        icon: "mdi-application",
-        title: "Desenvolvimento Web ou Desktop",
-        subtitle:
-          "Sistemas personalizados para ajudar nas metas do seu negócio."
-      },
-      {
-        icon: "mdi-airplane-landing",
-        title: "Landing Pages",
-        subtitle: "Direcione o público direto para a sua página exclusiva."
-      },
-      {
-        icon: "mdi-cellphone",
-        title: "Desenvolvimento de Aplicativos",
-        subtitle: "Aplicativos feitos para os principais dispositivos do mercado."
-      },
-      {
-        icon: "mdi-lightbulb",
-        title: "Consultoria em Projetos/T.I",
-        subtitle:
-          "Estamos prontos para te ajudar a tirar o seu projeto do papel!"
-      },
-      {
-        icon: "mdi-cart-plus",
-        title: "Lojas Virtuais/E-commerce",
-        subtitle: "Venda seus produtos para qualquer parte do mundo!"
-      },
-      {
-        icon: "mdi-wordpress",
-        title: "Suporte Wordpress",
-        subtitle: "Total suporte para o seu site ou sistema feito em wordpress."
-      }
-      // {
-      //   icon: "mdi-check-decagram",
-      //   title: "Redes Sociais",
-      //   subtitle: "Gerenciamento e configuração das principais redes sociais"
-      // },
-      // {
-      //   icon: "mdi-palette",
-      //   title: "Criação de Artes Digitais",
-      //   subtitle: "Artes digitais personalizadas utilizando I.A"
-      // },
-      // {
-      //   icon: "mdi-marker-check",
-      //   title: "Marketing Digital",
-      //   subtitle: "Criação e acompanhamento de campanhas digitais "
-      // },
-      // {
-      //   icon: "mdi-compass-rose",
-      //   title: "Identidade visual",
-      //   subtitle: "Criação da identidade da sua marca ou empresa"
-      // }
-    ]);
-
-    const dialogVisible = ref(false);
-    const currentItem = ref({});
-
-    const openDialog = (index) => {
-      currentItem.value = cards.value[index];
-      dialogVisible.value = true;
-    };
-
-    return {
-      cards,
-      dialogVisible,
-      currentItem,
-      openDialog,
-    };
+<script setup>
+import { ref } from "vue";
+const cards = ref([
+  {
+    icon: "mdi-application",
+    title: "Desenvolvimento Web ou Desktop",
+    subtitle: "Sistemas personalizados para ajudar nas metas do seu negócio.",
   },
-});
+  {
+    icon: "mdi-airplane-landing",
+    title: "Landing Pages",
+    subtitle: "Direcione o público direto para a sua página exclusiva.",
+  },
+  {
+    icon: "mdi-cellphone",
+    title: "Desenvolvimento de Aplicativos",
+    subtitle: "Aplicativos feitos para os principais dispositivos do mercado.",
+  },
+  {
+    icon: "mdi-lightbulb",
+    title: "Consultoria em Projetos/T.I",
+    subtitle: "Estamos prontos para te ajudar a tirar o seu projeto do papel!",
+  },
+  {
+    icon: "mdi-cart-plus",
+    title: "Lojas Virtuais/E-commerce",
+    subtitle: "Venda seus produtos para qualquer parte do mundo!",
+  },
+  {
+    icon: "mdi-wordpress",
+    title: "Suporte Wordpress",
+    subtitle: "Total suporte para o seu site ou sistema feito em wordpress.",
+  },
+  // {
+  //   icon: "mdi-check-decagram",
+  //   title: "Redes Sociais",
+  //   subtitle: "Gerenciamento e configuração das principais redes sociais"
+  // },
+  // {
+  //   icon: "mdi-palette",
+  //   title: "Criação de Artes Digitais",
+  //   subtitle: "Artes digitais personalizadas utilizando I.A"
+  // },
+  // {
+  //   icon: "mdi-marker-check",
+  //   title: "Marketing Digital",
+  //   subtitle: "Criação e acompanhamento de campanhas digitais "
+  // },
+  // {
+  //   icon: "mdi-compass-rose",
+  //   title: "Identidade visual",
+  //   subtitle: "Criação da identidade da sua marca ou empresa"
+  // }
+]);
+
+const dialogVisible = ref(false);
+const currentItem = ref({});
+
+const openDialog = (index) => {
+  currentItem.value = cards.value[index];
+  dialogVisible.value = true;
+};
 </script>
