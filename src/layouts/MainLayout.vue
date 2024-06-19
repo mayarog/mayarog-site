56f28d04b7ae711ff7ae2bcf04c619b34b6fa0b8
imLeonam <leonammrib@gmail.com>
2024-06-19 20:07:18 -0300

    refactoe(layouts): adding motion and removing unitilized code in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,29 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
-    <TheHeader />
+    <TheHeader v-motion-slide-visible-top :duration="700" />
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-<script>
-import { defineComponent, ref } from "vue";
+
+<script setup>
 import TheHeader from "src/components/TheHeader.vue";
-export default defineComponent({
-  name: "MainLayout",
-  components: {
-    TheHeader
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      }
-    };
-  }
-});
 </script>
