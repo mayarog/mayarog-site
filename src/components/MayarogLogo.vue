47b2a290c05ae0672a113337dd1c709b9df0df83
imLeonam <leonammrib@gmail.com>
2024-06-19 20:05:08 -0300

    feat(components): created MayarogLogo

---
+++ src/components/MayarogLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-start">
+    <div class="flex flex-center">
+      <q-img
+        class="q-ml-lg"
+        alt="Mayarog Web"
+        src="~assets/mayarog-logo.png"
+        style="width: 80px; height: 80px"
+      />
+      <span class="font--playfair-display text-h4"> Mayarog </span>
+    </div>
+  </div>
+</template>
