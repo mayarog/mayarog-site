61f7fecf5fa85791904a02884c5fb70396271c29
imLeonam <leonammrib@gmail.com>
2024-06-20 20:20:10 -0300

    feat(layouts): added shorter duration in animation of TheHeader in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
-    <TheHeader v-motion-slide-visible-top :duration="700" />
+    <TheHeader v-motion-slide-visible-top :duration="500" />
     <q-page-container>
       <router-view />
     </q-page-container>
