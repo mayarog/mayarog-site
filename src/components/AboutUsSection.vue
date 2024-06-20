f7a914428c8029c339630de3982bd323594be6a6
imLeonam <leonammrib@gmail.com>
2024-06-20 20:11:20 -0300

    fix(components): fixed responsivity and animations in AboutUsSection

--- src/components/AboutUsSection.vue
+++ src/components/AboutUsSection.vue
@@ -1,7 +1,11 @@
 <template>
-  <q-page-container id="services" class="bg-dark text-white text-center">
-    <div class="row flex flex-center" v-motion-slide-visible-once-left>
-      <div class="col-12 q-px-lg">
+  <q-page-container class="bg-dark text-white text-center">
+    <div
+      class="row flex flex-center"
+      v-motion-slide-visible-once-left
+      :duration="500"
+    >
+      <div class="col-12">
         <h2 class="text-h2 font-customization">
           Somos a <span class="text-primary">Mayarog Web</span>, onde a
           <br class="desktop-only" />
@@ -12,41 +16,41 @@
           prosperam.
         </h2>
       </div>
-      <div class="col-md-6 col-xs-12 col-sm-12 q-px-xl">
+      <!-- <div class="col-lg-6 col-md-12 col-xs-12 col-sm-12 q-mb-xl">
         <h1
-          class="text-primary text-left q-ma-lg font-customization"
-          v-motion-slide-visible-once-left
+          class="text-primary text-left font-customization"
+          v-motion-slide-visible-once-bottom
           :delay="200"
-          :duration="700"
+          :duration="500"
         >
           Quem somos
         </h1>
         <p
-          class="text-h4 text-left q-ma-lg q-pt-md"
-          v-motion-slide-visible-once-left
+          class="text-h4 text-left"
+          v-motion-slide-visible-once-bottom
           :delay="300"
-          :duration="700"
+          :duration="500"
         >
           Somos uma startup apaixonada por desenvolvimento e consultoria em
           Soluções WEB, dedicada a impulsionar o sucesso dos nossos clientes por
           meio de soluções digitais personalizadas e eficazes.
         </p>
         <p
-          class="text-h4 text-left q-ma-lg"
-          v-motion-slide-visible-once-left
+          class="text-h4 text-left"
+          v-motion-slide-visible-once-bottom
           :delay="400"
-          :duration="700"
+          :duration="500"
         >
           Na Mayarog, entendemos que cada projeto é único e merece uma abordagem
           personalizada. É por isso que oferecemos uma ampla gama de serviços
           para atender às suas necessidades específicas.
         </p>
-      </div>
-      <div class="col-6 mobile-hide">
+      </div> -->
+      <!-- <div class="col-6 mobile-hide">
         <q-img
           v-motion-slide-visible-once-right
           :delay="200"
-          :duration="700"
+          :duration="500"
           class="rounded"
           rounded
           src="https://media-public.canva.com/l7-i4/MAEFJQl7-i4/1/s2.jpg"
@@ -54,8 +58,12 @@
           height="28vw"
           style="z-index: 1; min-width: 420px; min-height: 420px"
         />
-      </div>
-      <div class="col-12">
+      </div> -->
+      <div
+        class="col-12 q-mt-xl"
+        v-motion-slide-visible-once-bottom
+        :duration="500"
+      >
         <q-btn
           v-motion
           :initial="{ opacity: 0, y: 100 }"
