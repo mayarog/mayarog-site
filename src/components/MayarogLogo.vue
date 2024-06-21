8bf3a6f37059a6f9b8cc909476a12e568d285135
imLeonam <leonammrib@gmail.com>
2024-06-21 10:44:35 -0300

    feat(components): added size prop in MayarogLogo

--- src/components/MayarogLogo.vue
+++ src/components/MayarogLogo.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="flex flex-start">
-    <div class="flex flex-center">
+    <div
+      class="flex flex-center"
+      :style="{ maxWidth: `${Number(props.size) * 4}px` }"
+    >
       <q-img
         class="q-ml-lg"
         alt="Mayarog Web"
         src="~assets/mayarog-logo.png"
-        style="width: 80px; height: 80px"
+        :style="{ width: `${props.size}px`, height: `${props.size}px` }"
       />
-      <span class="font--playfair-display text-h4"> Mayarog </span>
+      <span
+        class="font--playfair-display text-h4"
+        :style="{ fontSize: `${Number(props.size) / 2}px` }"
+      >
+        Mayarog
+      </span>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  size: { type: String, default: "80" },
+});
+</script>
