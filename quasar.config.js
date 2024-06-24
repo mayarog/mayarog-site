7ab78e7a8752fdab8eee0f33267b1effc4157e3f
imLeonam <leonammrib@gmail.com>
2024-06-24 12:07:48 -0300

    feat(quasar): added Dialog plugins in quasar.config

--- quasar.config.js
+++ quasar.config.js
@@ -105,7 +105,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Dialog'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
