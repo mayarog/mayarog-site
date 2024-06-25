f6fb352cdab02dc346610aed9ae21b763281aa81
imLeonam <leonammrib@gmail.com>
2024-06-25 11:57:06 -0300

    feat(layouts): added HeaderSectionMobile in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,15 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
-    <HeaderSection v-motion-slide-visible-top :duration="500" />
+    <HeaderSection
+      class="desktop-only"
+      v-motion-slide-visible-top
+      :duration="500"
+    />
+    <HeaderSectionMobile
+      class="mobile-only"
+      v-motion-slide-visible-top
+      :duration="500"
+    />
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -9,4 +18,5 @@
 
 <script setup>
 import HeaderSection from "src/components/HeaderSection.vue";
+import HeaderSectionMobile from "src/components/HeaderSectionMobile.vue";
 </script>
