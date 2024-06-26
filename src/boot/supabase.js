cb91b37bc44520b65b5e404d05527c1e263a4935
imLeonam <leonammrib@gmail.com>
2024-06-26 14:40:20 -0300

    feat(boot): exporting boot/supabase

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,13 +1,15 @@
 import { boot } from 'quasar/wrappers'
 import { createClient } from '@supabase/supabase-js'
-import('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 export default boot(({ app }) => {
-  const supabase = createClient(supabaseUrl, supabaseKey);
 
   app.config.globalProperties.$supabase = supabase;
-})
+});
+
+export { supabase };
