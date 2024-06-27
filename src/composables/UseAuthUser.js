f94db3bebf060bbfbe20d82fc5cbff2b6fe5355e
imLeonam <leonammrib@gmail.com>
2024-06-27 14:21:10 -0300

    feat(composables): created UseAuthUser

---
+++ src/composables/UseAuthUser.js
@@ -0,0 +1,63 @@
+import { ref } from 'vue';
+import useSupabase from 'boot/supabase';
+
+const user = ref(null);
+export default function useAuthUser() {
+  const { supabase } = useSupabase();
+
+  const login = async ({ email, password }) => {
+    const { user, error } = await supabase.auth.signIn({ email, password });
+    if (error) throw error;
+    return user;
+  };
+
+  const loginWithSocialProvider = async (provider) => {
+    const { user, error } = await supabase.auth.signIn({ provider });
+    if (error) throw error;
+    return user;
+  };
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+  };
+
+  const isLoggedIn = async () => {
+    return !!user.value;
+  }
+
+  const register = async ({ email, password, ...meta }) => {
+    const { user, error } = await supabase.auth.signUp(
+      { email, password },
+      {
+        data: meta,
+        redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`,
+      },
+    );
+    if (error) throw error;
+    return user;
+  };
+
+  const update = async (data) => {
+    const { user, error } = await supabase.auth.update(data);
+    if (error) throw error;
+    return user;
+  };
+
+  const sendPasswordRestEmail = async (email) => {
+    const { user, error } = await supabase.auth.api.resetPasswordForEmail(email);
+    if (error) throw error;
+    return user;
+  };
+
+  return {
+    user,
+    login,
+    loginWithSocialProvider,
+    logout,
+    isLoggedIn,
+    register,
+    update,
+    sendPasswordRestEmail,
+  }
+};
