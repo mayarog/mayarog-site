a3a1efdc539c1c4adb2385b8f020fed9bed3c521
imLeonam <leonammrib@gmail.com>
2024-06-27 14:19:59 -0300

    feat(layouts): created AuthLayout

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="shadow-2 rounded-borders">
+    <HeaderSection v-motion-slide-visible-top :duration="500" hideButtons />
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import HeaderSection from "src/components/HeaderSection.vue";
+import HeaderSectionMobile from "src/components/HeaderSectionMobile.vue";
+</script>
