77111d3d58b9d7629134c4f972dcf577b9347704
imLeonam <leonammrib@gmail.com>
2024-06-27 14:15:02 -0300

    refactor(boot): changed supabase export in supabase

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,15 +1,9 @@
-import { boot } from 'quasar/wrappers'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default boot(({ app }) => {
-
-  app.config.globalProperties.$supabase = supabase;
-});
-
-export { supabase };
+export default function useSupabase() {
+  return { supabase };
+};
