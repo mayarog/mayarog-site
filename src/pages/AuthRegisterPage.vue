4d4352d434394043b560ea1e7b29a005d283ee44
imLeonam <leonammrib@gmail.com>
2024-06-28 13:07:01 -0300

    refactor(pages): refactored structure in AuthRegisterPage

--- src/pages/AuthRegisterPage.vue
+++ src/pages/AuthRegisterPage.vue
@@ -1,143 +1,131 @@
 <template>
-  <div class="content" v-motion-slide-visible-once-bottom :duration="500">
-    <div>
-      <q-form @submit.prevent="handleRegister">
-        <div>
-          <div class="row q-pa-md text-center">
-            <div class="col-12">
-              <h1 class="text-h1 text-primary font-customization">
-                Área do cliente
-              </h1>
-            </div>
-            <div class="col-12">
-              <h4 class="text-h4">Preencha os campos para se cadastrar</h4>
-            </div>
-          </div>
-          <div class="row inputs-container">
-            <div class="col-12 q-ma-sm">
-              <q-input
-                rounded
-                outlined
-                v-model="form.name"
-                :rules="rules.name"
-                color="primary"
-                type="text"
-                label="Nome"
-              />
-            </div>
-            <div class="col-12 q-ma-sm">
-              <q-input
-                rounded
-                outlined
-                debounce="1000"
-                v-model="form.email"
-                @change="updateEmail"
-                :rules="rules.email"
-                color="primary"
-                type="email"
-                label="Email"
+  <q-page padding v-motion-slide-visible-once-bottom :duration="500">
+    <q-form @submit.prevent="handleRegister" class="row justify-center">
+      <p class="col-12 text-h2 text-center text-primary font-customization">
+        <strong>Área do cliente</strong>
+      </p>
+      <p class="col-12 text-h5 text-center">
+        Preencha os campos para se cadastrar
+      </p>
+      <div class="col-md-4 col-sm-6 col-xs-10">
+        <q-input
+          rounded
+          outlined
+          v-model="form.name"
+          :rules="rules.name"
+          color="primary"
+          type="text"
+          label="Nome"
+        />
+        <q-input
+          rounded
+          outlined
+          v-model="form.phone"
+          :rules="rules.phone"
+          color="primary"
+          mask="## # ####-####"
+          type="text"
+          label="Telefone"
+        />
+        <q-input
+          rounded
+          outlined
+          debounce="1000"
+          v-model="form.email"
+          @change="updateEmail"
+          :rules="rules.email"
+          color="primary"
+          type="email"
+          label="Email"
+        >
+          <template #hint>
+            <p v-if="isProviderCorrect"></p>
+            <p v-else-if="form.email && suggestedProvider">
+              Você quis dizer
+              <strong
+                style="cursor: pointer"
+                @click="fixProvider(suggestedProvider)"
               >
-                <template #hint>
-                  <p v-if="isProviderCorrect"></p>
-                  <p v-else-if="form.email && suggestedProvider">
-                    Você quis dizer
-                    <strong
-                      style="cursor: pointer"
-                      @click="fixProvider(suggestedProvider)"
-                    >
-                      {{ suggestedProvider }}
-                    </strong>
-                    ?
-                  </p>
-                </template>
-              </q-input>
-            </div>
-            <div class="col-12 q-ma-sm">
-              <q-input
-                rounded
-                outlined
-                v-model="form.password"
-                :rules="rules.password"
-                color="primary"
-                :type="fields.password.isPwd ? 'password' : 'text'"
-                label="Senha"
-              >
-                <template #hint>
-                  <q-linear-progress
-                    v-if="form.password"
-                    rounded
-                    size="4px"
-                    :value="passwordStrength(form.password).score"
-                    :color="passwordStrength(form.password).color"
-                    class="q-mt-sm"
-                  />
-                </template>
-                <template #append>
-                  <q-icon
-                    class="q-mr-sm"
-                    @click="fields.password.isPwd = !fields.password.isPwd"
-                    :name="
-                      fields.password.isPwd ? 'visibility' : 'visibility_off'
-                    "
-                  />
-                </template>
-              </q-input>
-            </div>
-            <div class="col-12 q-ma-sm">
-              <q-input
-                rounded
-                outlined
-                v-model="confirmPassword"
-                :rules="rules.confirmPassword"
-                color="primary"
-                :type="fields.confirmPassword.isPwd ? 'password' : 'text'"
-                label="Confirmar Senha"
-              >
-                <template #append>
-                  <q-icon
-                    class="q-mr-sm"
-                    @click="
-                      fields.confirmPassword.isPwd =
-                        !fields.confirmPassword.isPwd
-                    "
-                    :name="
-                      fields.confirmPassword.isPwd
-                        ? 'visibility'
-                        : 'visibility_off'
-                    "
-                  />
-                </template>
-              </q-input>
-            </div>
-          </div>
-        </div>
-
+                {{ suggestedProvider }}
+              </strong>
+              ?
+            </p>
+          </template>
+        </q-input>
+        <q-input
+          rounded
+          outlined
+          v-model="form.password"
+          :rules="rules.password"
+          color="primary"
+          :type="fields.password.isPwd ? 'password' : 'text'"
+          label="Senha"
+        >
+          <template #hint>
+            <q-linear-progress
+              v-if="form.password"
+              rounded
+              size="4px"
+              :value="passwordStrength(form.password).score"
+              :color="passwordStrength(form.password).color"
+              class="q-mt-sm"
+            />
+          </template>
+          <template #append>
+            <q-icon
+              class="q-mr-sm"
+              @click="fields.password.isPwd = !fields.password.isPwd"
+              :name="fields.password.isPwd ? 'visibility' : 'visibility_off'"
+            />
+          </template>
+        </q-input>
+        <q-input
+          rounded
+          outlined
+          v-model="confirmPassword"
+          :rules="rules.confirmPassword"
+          color="primary"
+          :type="fields.confirmPassword.isPwd ? 'password' : 'text'"
+          label="Confirmar Senha"
+        >
+          <template #append>
+            <q-icon
+              class="q-mr-sm"
+              @click="
+                fields.confirmPassword.isPwd = !fields.confirmPassword.isPwd
+              "
+              :name="
+                fields.confirmPassword.isPwd ? 'visibility' : 'visibility_off'
+              "
+            />
+          </template>
+        </q-input>
         <div class="row">
-          <div class="col-6 q-pa-md q-gutter-lg">
+          <div class="col-6">
             <q-btn
               rounded
               style="width: fit-content"
-              class="text--dark q-py-sm q-px-xl"
+              class="text--dark"
               color="primary"
               text-color="dark"
               type="submit"
             >
-              <strong class="text-h6">Cadastrar-se</strong>
+              <strong>Cadastrar-se</strong>
             </q-btn>
           </div>
-          <div class="col-6 q-mt-sm text-right">
-            <strong class="text-h6 text-white">Já possui conta?</strong><br />
+          <div class="col-6 text-right">
+            <p class="text-h5 text-white">Já possui conta?</p>
             <router-link
               style="text-decoration: none"
               :to="{ name: 'auth-login' }"
             >
-              <strong class="text-h6 text-primary">Clique aqui</strong>
+              <strong class="text-h5 text-primary">Clique aqui</strong>
             </router-link>
           </div>
         </div>
-      </q-form>
-    </div>
-  </div>
+      </div>
+    </q-form>
+  </q-page>
 </template>
 
 <script setup>
@@ -150,6 +138,7 @@ const { register } = useAuthUser();
 
 const rules = {
   name: [(value) => !!value || "O nome é obrigatório"],
+  phone: [(value) => !!value || "O telefone é obrigatório"],
   email: [
     (value) => !!value || "O e-mail é obrigatório",
     (value) =>
@@ -164,6 +153,7 @@ const rules = {
 
 const form = reactive({
   name: "",
+  phone: "",
   email: "",
   password: "",
 });
@@ -245,23 +235,9 @@ const fields = reactive({
 </script>
 
 <style scoped>
-.inputs-container {
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  margin-bottom: 20px;
-}
-.inputs-container div {
-  max-width: 600px;
-}
-
-.content {
+.container {
   display: flex;
   justify-content: center;
   align-items: center;
-}
-
-.content > div {
-  width: 650px;
 }
 </style>
