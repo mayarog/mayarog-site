0cb23a68fdf9049dbb82f5dd063c415d25260a7a
imLeonam <leonammrib@gmail.com>
2024-06-28 13:37:12 -0300

    feat(pages): created EmailConfirmation

---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding class="text-center">
+    <p class="text-body1">Obrigado por se registrar !</p>
+    <p class="text-body1">
+      Por favor confirme seu email para finalizar seu cadastro:
+      <strong>{{ $route.query.email }}</strong>
+    </p>
+    <q-btn
+      round
+      outline
+      color="red"
+      icon="mail"
+      size="lg"
+      style="margin: 10px"
+      href="https://mail.google.com/mail/u/0/#search/Mayarog+-+Confirmar+Cadastro"
+      target="_blank"
+    />
+  </q-page>
+</template>
+
+<script setup></script>
