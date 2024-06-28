17c266c11b8bb71d78f2724f25cab42a2bf52a19
imLeonam <leonammrib@gmail.com>
2024-06-28 13:37:19 -0300

    feat(pages): created Me

---
+++ src/pages/Me.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <div v-if="user">
+      <p>Olá {{ user.user_metadata.name }}</p>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import useAuthUser from "src/composables/UseAuthUser";
+defineOptions({
+  name: "MePage",
+});
+
+const { user } = useAuthUser();
+</script>
